56eb6a3b9fe36c4f24e4f6533bd09d8fb539723b
Canberk Elmal <elmalcanberk@gmail.com>
2023-01-03 00:40:23 +0300

    PlusRemoveParticleEffects

--- Assets/RemoveGateSc.cs
+++ Assets/RemoveGateSc.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class RemoveGateSc : MonoBehaviour
 {
+    public GameObject removeEffect;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ball")
         {
+            Instantiate(removeEffect, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
         }
     }
--- Assets/Scripts/PlusWallSc.cs
+++ Assets/Scripts/PlusWallSc.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlusWallSc : MonoBehaviour
 {
     GameObject balls;
-    public int numberOfBalls = 1;
+    public int numberOfBalls = 3;
+    public GameObject plusEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +28,7 @@ public class PlusWallSc : MonoBehaviour
     void BallSpawner(GameObject theBall)
     {
         Vector3 insTarget = new Vector3(theBall.transform.position.x + Random.Range(-0.5f , 0.5f), theBall.transform.position.y + Random.Range(-0.5f , 0.5f),0);
+        Instantiate(plusEffect, insTarget, Quaternion.identity);
         Instantiate(theBall, insTarget, theBall.transform.rotation, balls.transform);
     }
 }
